97c47523105028edba2fc60d2ec3b56780bbc2a4
f1781600921 <1781600921@qq.com>
2023-12-02 20:16:03 +0800

    update

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_IHU510G.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_IHU510G-user \
+    omni_IHU510G-userdebug \
+    omni_IHU510G-eng
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# android_device_IHU510G
- 吉利缤瑞
+# Android device tree for ecarx FE6A3 (IHU510G)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
